ed8ed41b74beef17c64787cc43718679bc80091e
seabbs <s.e.abbott12@gmail.com>
2021-01-10 18:21:03 +0000

    Add var export

--- setup/env-var.sh
+++ setup/env-var.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 
-MOUNT_NETRC=type=bind,source=$(realpath ~)/.netrc,target=/root/.netrc,readonly
-MOUNT_GITCONFIG=type=bind,source=$(realpath ~)/.gitconfig,target=/root/.gitconfig,readonly
+export MOUNT_NETRC=type=bind,source=$(realpath ~)/.netrc,target=/root/.netrc,readonly
+export MOUNT_GITCONFIG=type=bind,source=$(realpath ~)/.gitconfig,target=/root/.gitconfig,readonly
