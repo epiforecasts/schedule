4bcf8c4aa9d9573116da022c4d3a29a0722c3954
seabbs <s.e.abbott12@gmail.com>
2021-01-10 18:18:37 +0000

    Correct mounting

--- setup/epinow2-covid-forecast.sh
+++ setup/epinow2-covid-forecast.sh
@@ -5,7 +5,7 @@ bash schedule/setup/env-var.sh
 # set up EpiNow2 based docker container for forecasting
 sudo docker pull docker.pkg.github.com/epiforecasts/epinow2/epinow2:latest
 sudo docker tag docker.pkg.github.com/epiforecasts/epinow2/epinow2:latest forecast
-sudo docker run -d -p 12003:8787 --name forecast -mount ${MOUNT_NETRC} --mount ${MOUNT_GITCONFIG} -e USER=forecast -e PASSWORD=forecast forecast
+sudo docker run -d -p 12003:8787 --name forecast --mount ${MOUNT_NETRC} --mount ${MOUNT_GITCONFIG} -e USER=forecast -e PASSWORD=forecast forecast
 # clone in US forecasting repository
 sudo docker exec forecast git clone https://github.com/epiforecasts/covid-us-forecasts.git
 # clone in Germany/Poland forecasting repository
--- setup/spim.sh
+++ setup/spim.sh
@@ -6,6 +6,6 @@ bash schedule/setup/env-var.sh
 sudo docker pull docker.pkg.github.com/epiforecasts/covid-uk/coviduk:latest
 sudo docker tag docker.pkg.github.com/epiforecasts/covid-uk/coviduk:latest spim
 # launch container
-sudo docker run -d -p 12002:8787 --name spim -mount ${MOUNT_NETRC} --mount ${MOUNT_GITCONFIG} -e USER=spim -e PASSWORD=spim spim
+sudo docker run -d -p 12002:8787 --name spim --mount ${MOUNT_NETRC} --mount ${MOUNT_GITCONFIG} -e USER=spim -e PASSWORD=spim spim
 # clone in repository
 sudo docker exec spim git clone https://github.com/epiforecasts/covid-uk.git
