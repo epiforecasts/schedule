cbf34bb95a1dd0ae05424a0abc95fd35309c4146
seabbs <s.e.abbott12@gmail.com>
2021-01-10 15:58:49 +0000

    Add initial setup

---
+++ setup/docker.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+apt-get update
+apt-get install -y docker.io
+docker login docker.pkg.github.com
---
+++ setup/epinow2.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+sudo docker pull docker.pkg.github.com/epiforecasts/epinow2/epinow2:latest
+sudo docker tag docker.pkg.github.com/epiforecasts/epinow2/epinow2:latest epinow2
+sudo docker run -d -p $1:8787 --name $2 -e USER=$2 -e PASSWORD=$2 epinow2
