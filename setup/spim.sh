677bcb9b280244c3ff40144b4fa5707534fbb35a
seabbs <s.e.abbott12@gmail.com>
2021-01-11 09:47:12 +0000

    resolve SPIM container code location issue.

--- setup/spim.sh
+++ setup/spim.sh
@@ -7,5 +7,7 @@ docker pull docker.pkg.github.com/epiforecasts/covid-uk/coviduk:latest
 docker tag docker.pkg.github.com/epiforecasts/covid-uk/coviduk:latest spim
 # launch container
 docker run -d -p 12002:8787 --name spim --mount ${MOUNT_NETRC} --mount ${MOUNT_GITCONFIG} -e USER=spim -e PASSWORD=spim spim
+# remove old code
+docker exec -w /home/rstudio spim rm -r -f covid-uk
 # clone in repository
-docker exec spim git clone https://github.com/epiforecasts/covid-uk.git
+docker exec -w /home/rstudio spim git clone https://github.com/epiforecasts/covid-uk.git
