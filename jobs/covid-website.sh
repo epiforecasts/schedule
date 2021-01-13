84aef93d323b7bbfe729e1ea70fcb8fd5d2be619
Sam Abbott <s.e.abbott12@gmail.com>
2021-01-13 17:51:02 +0000

    Update covid-website.sh

--- jobs/covid-website.sh
+++ jobs/covid-website.sh
@@ -1,4 +1,7 @@
 #!bin/bash
 
+# update dependencies
+docker exec -w /home/rstudio/covid covid-website Rscript -e "devtools::install_dev_deps()"
+
 # run the updater script stored in covid/bin/update.sh
 docker exec -w /home/rstudio/covid covid-website bash bin/update.sh
