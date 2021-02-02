5bc13cb728ff35357307bda1f2caef7ae9cd375d
Sam Abbott <s.e.abbott12@gmail.com>
2021-02-02 17:20:24 +0000

    update crowd

--- jobs/germany-covid-forecast/crowd.sh
+++ jobs/germany-covid-forecast/crowd.sh
@@ -1,25 +1,16 @@
 #!bin/bash
 
 # check repository is up to date and update dependencies
-/bin/bash schedule/jobs/germany-covid-forecasts/update-dependencies.sh
+. schedule/jobs/germany-covid-forecasts/pull.sh
 
-# create crowd forecast app submission data
-docker exec -w /home/rstudio/covid-german-forecasts forecast Rscript human-forecast/update-submissions.R
+# update crowd forecast
+docker exec -w /home/rstudio/covid-german-forecasts forecast bash human-forecast/update.sh
 
-# Redeploy forecast app (to update submission date to next week)
-docker exec -w /home/rstudio/covid-german-forecasts forecast Rscript human-forecast/redeploy-forecast-app.R
+# update Rt crowd forecast 
+docker exec -w /home/rstudio/covid-german-forecasts forecast bash rt-crowd-forecast/update.sh
 
-# update Rt crowd forecast samples
-docker exec -w /home/rstudio/covid-german-forecasts forecast Rscript rt-crowd-forecast/update-samples.R
-
-# simulate cases from Rt crowd forecast
-docker exec -w /home/rstudio/covid-german-forecasts forecast Rscript rt-crowd-forecast/simulate-cases.R
-
-# Redeploy Rt forecast app (to update submission date to next week)
-docker exec -w /home/rstudio/covid-german-forecasts forecast Rscript rt-crowd-forecast/redeploy-rt-crowd-forecast-app.R
-
-# rerun evaluation / leaderboard
-docker exec -w /home/rstudio/covid-german-forecasts forecast Rscript evaluation/forecast-evaluation.R
+# update evaluations
+docker exec -w /home/rstudio/covid-german-forecasts forecast bash evaluation/update.sh
 
 # update github with new forecast
 docker exec -w /home/rstudio/covid-german-forecasts forecast  bash -c "git add -A ; git commit -m 'automated crowd update' ; git pull -Xours; git push"
