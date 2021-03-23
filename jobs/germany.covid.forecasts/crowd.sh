bc251432a33abbcf9c4b6f32681c2c68edef2aab
Nikos Bosse <37978797+nikosbosse@users.noreply.github.com>
2021-03-23 13:19:15 +0100

    add evaluation
    add evaluation

--- jobs/germany.covid.forecasts/crowd.sh
+++ jobs/germany.covid.forecasts/crowd.sh
@@ -9,5 +9,8 @@ docker exec -w /home/rstudio/covid.german.forecasts forecast bash crowd-forecast
 # update Rt crowd forecast 
 docker exec -w /home/rstudio/covid.german.forecasts forecast bash rt-crowd-forecast/update.sh
 
+# update evaluation
+docker exec -w /home/rstudio/covid.german.forecasts forecast bash evaluation/update.sh
+
 # update github with new forecast
 docker exec -w /home/rstudio/covid.german.forecasts forecast  bash -c "git add -A ; git commit -m 'automated crowd update' ; git pull -Xours; git push"
