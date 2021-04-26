b95346ef1643bbe73cd5450ba952f81851cf336d
Sam Abbott <s.e.abbott12@gmail.com>
2021-04-26 11:00:48 +0000

    drop test run

--- crontab.sh
+++ crontab.sh
@@ -32,6 +32,5 @@
 30 7 * * 0 /bin/bash schedule/jobs/europe-covid-forecast/crowd-data.sh > schedule/logs/europe-covid-forecast/crowd-data.log 2>&1
 # update European crowd forecast submission on Monday at 10 pm
 5 21 * * 1 /bin/bash schedule/jobs/europe-covid-forecast/crowd.sh > schedule/logs/europe-covid-forecast/crowd.log 2>&1
-# run all SPIM tasks at 7am on Monday (as a test) and 2am on Tuesday (for SPI-M submission)
-0 7 * * 1 /bin/bash schedule/jobs/spim.sh > schedule/logs/spim.log 2>&1
+# run all SPIM tasks at 2am on Tuesday (for SPI-M submission)
 0 2 * * 2 /bin/bash schedule/jobs/spim.sh > schedule/logs/spim.log 2>&1
