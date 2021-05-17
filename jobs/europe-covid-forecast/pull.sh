df7ad15a31569fbfba07af071a8e1d125b7311b9
seabbs <s.e.abbott12@gmail.com>
2021-05-17 10:57:41 +0100

    change to local install

--- jobs/europe-covid-forecast/pull.sh
+++ jobs/europe-covid-forecast/pull.sh
@@ -4,5 +4,5 @@
 docker exec -w /home/rstudio/europe-covid-forecast forecast git pull -Xours
 
 # update dependencies
-docker exec -w /home/rstudio/europe-covid-forecast forecast Rscript -e 'devtools::install_github("epiforecasts/europe-covid-forecast")'
 docker exec -w /home/rstudio/europe-covid-forecast forecast Rscript -e 'devtools::install_dev_deps()'
+docker exec -w /home/rstudio/europe-covid-forecast forecast Rscript -e 'devtools::install(".")'
