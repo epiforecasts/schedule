98d86663bfdd61ffa3f3667a1bc51a8ae61b7f4f
Sam Abbott <s.e.abbott12@gmail.com>
2021-08-09 09:39:16 +0000

    add restart protection

--- jobs/germany.covid.forecasts/pull.sh
+++ jobs/germany.covid.forecasts/pull.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
+# make sure required container is running
+docker start forecast
+
 # check repository is up to date
 docker exec -w /home/rstudio/covid.german.forecasts forecast git pull -Xours
 
--- jobs/us-covid-forecast.sh
+++ jobs/us-covid-forecast.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
+# make sure required container is running
+docker start forecast
+
 # check repository is up to date
 docker exec -w /home/rstudio/covid-us-forecasts forecast git pull -Xours
 
