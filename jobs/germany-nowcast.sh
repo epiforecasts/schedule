12d5093cadeda0eafa900afbf537273d509df832
Sam Abbott <s.e.abbott12@gmail.com>
2021-11-26 09:45:58 +0000

    correct permissions changes for nowcasting

--- jobs/germany-nowcast.sh
+++ jobs/germany-nowcast.sh
@@ -4,7 +4,8 @@
 docker start frosty_roentgen
 
 # make sure it has the right permissions and is up to date
-docker exec -w /eval-germany-sp-nowcasting frosty_roentgen bash sudo  chmod -R ugo+rw . && git pull
+docker exec -w /eval-germany-sp-nowcasting frosty_roentgen chmod -R ugo+rw .
+docker exec -w /eval-germany-sp-nowcasting frosty_roentgen git pull
 
 # run the updater script
 docker exec -w /eval-germany-sp-nowcasting frosty_roentgen  bash bin/update-targets-and-publish.sh
