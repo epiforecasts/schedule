22393ae19fb091ec2b3d4385c1070555302c1b48
Sam Abbott <s.e.abbott12@gmail.com>
2021-11-28 22:03:44 +0000

    revert to vscode container for today

--- jobs/germany-nowcast.sh
+++ jobs/germany-nowcast.sh
@@ -1,14 +1,13 @@
 #!bin/bash
 
 # make sure required container is running
-docker start eval-germany-sp-nowcasting
+docker start frosty_roentgen
 
 # make sure it has the right permissions and is up to date
-docker exec -w /eval-germany-sp-nowcasting eval-germany-sp-nowcasting chmod -R ugo+rw .
-docker exec -u vscode -w /eval-germany-sp-nowcasting eval-germany-sp-nowcasting git pull
+docker exec -w /eval-germany-sp-nowcasting frosty_roentgen chmod -R ugo+rw .
+docker exec -u vscode -w /eval-germany-sp-nowcasting frosty_roentgen git pull
 
 # run the updater script
-docker exec -u vscode -w /eval-germany-sp-nowcasting eval-germany-sp-nowcasting  bash bin/update-targets-and-publish.sh
+docker exec -u vscode -w /eval-germany-sp-nowcasting frosty_roentgen  bash bin/update-targets-and-publish.sh
 
-# we need to do a manual push as the vscode user may not have git credentials
-docker exec  -w /eval-germany-sp-nowcasting eval-germany-sp-nowcasting git push
+docker exec  -w /eval-germany-sp-nowcasting frosty_roentgen  git push
