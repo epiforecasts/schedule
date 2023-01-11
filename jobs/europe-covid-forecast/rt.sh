ebff0d501ead677e5f395b747aef18237f7e9b54
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-01-11 14:48:45 +0000

    remove no longer used items from schedule

--- jobs/europe-covid-forecast/rt.sh
+++ jobs/europe-covid-forecast/rt.sh
@@ -6,8 +6,5 @@
 # run forecasts
 docker exec -w /home/rstudio/europe-covid-forecast forecast bash rt-forecast/update.sh
 
-# Redeploy crowd rt forecast app with updated data
-docker exec -w /home/rstudio/europe-covid-forecast forecast Rscript crowd-rt-forecast/redeploy.R
-
 # update github with new forecast
 docker exec -w /home/rstudio/europe-covid-forecast forecast  bash -c "git add -A ; git commit -m 'automated Rt update' ; git pull -Xours; git push"
