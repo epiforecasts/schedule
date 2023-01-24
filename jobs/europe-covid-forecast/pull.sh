7c495c753559c733a83c5ccc5f2b3506176031ed
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-01-24 17:49:04 +0000

    update epinow2 image to latest

--- jobs/europe-covid-forecast/pull.sh
+++ jobs/europe-covid-forecast/pull.sh
@@ -4,8 +4,8 @@
 docker start forecast
 
 # check repository is up to date
-docker exec -w /home/rstudio/europe-covid-forecast forecast git pull -Xours
+docker exec -w /home/vscode/europe-covid-forecast forecast git pull -Xours
 
 # update dependencies
-docker exec -w /home/rstudio/europe-covid-forecast forecast Rscript -e 'devtools::install_dev_deps()'
-docker exec -w /home/rstudio/europe-covid-forecast forecast Rscript -e 'devtools::install(".")'
+docker exec -w /home/vscode/europe-covid-forecast forecast Rscript -e 'devtools::install_dev_deps()'
+docker exec -w /home/vscode/europe-covid-forecast forecast Rscript -e 'devtools::install(".")'
