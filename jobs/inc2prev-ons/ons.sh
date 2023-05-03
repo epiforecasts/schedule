e530d3e45d5f3be99a21f86255aaa8963dc7545e
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-05-03 09:28:52 +0000

    rename inc2prev jobs

--- jobs/inc2prev-ons/ons.sh
+++ jobs/inc2prev-ons/ons.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 # check repository is up to date and update dependencies
-. schedule/jobs/inc2prev/pull.sh
+. schedule/jobs/inc2prev-ons/pull.sh
 
 # produce estimates
-docker exec -w /home/vscode/inc2prev inc2prev-obs bash scripts/update.sh
+docker exec -w /home/vscode/inc2prev inc2prev-ons bash scripts/update.sh
 
 # publish estimates
-docker exec -w /home/vscode/inc2prev inc2prev-obs bash scripts/publish.sh
+docker exec -w /home/vscode/inc2prev inc2prev-ons bash scripts/publish.sh
--- jobs/inc2prev-ons/pull.sh
+++ jobs/inc2prev-ons/pull.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
 # make sure required container is running
-docker start inc2prev-obs
+docker start inc2prev-ons
 
 # check repository is up to date
-docker exec -w /home/vscode/inc2prev-obs forecast git pull -Xours
+docker exec -w /home/vscode/inc2prev inc2prev-ons git pull -Xours
 
 # update dependencies
-docker exec -w /home/vscode/inc2prev-obs forecast Rscript -e 'devtools::install_dev_deps()'
-docker exec -w /home/vscode/inc2prev-obs forecast Rscript -e 'devtools::install(".")'
+docker exec -w /home/vscode/inc2prev inc2prev-ons Rscript -e 'devtools::install_dev_deps()'
+docker exec -w /home/vscode/inc2prev inc2prev-ons Rscript -e 'devtools::install(".")'
